a4bc68337e91cb1fa920ac64b68853625c8e8452
Anshul Noori <anshulnoori@gmail.com>
2026-07-19 18:52:07 -0700

    build: flake-parts skeleton with treefmt-nix formatter

--- flake.nix
+++ flake.nix
@@ -1,15 +1,46 @@
 {
-  description = "A very basic flake";
+  description = "Anshul's personal NixOS configuration";
+
+  nixConfig = {
+    extra-substituters = ["https://anshulnoori.cachix.org"];
+    extra-trusted-public-keys = [
+      # TODO: replace with the real key printed by `cachix use anshulnoori`
+      "anshulnoori.cachix.org-1:REPLACE_ME"
+    ];
+  };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+      perSystem = _: {
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs = {
+            alejandra.enable = true; # *.nix
+            prettier.enable = true; # *.md, *.json, *.yaml/*.yml
+            shfmt.enable = true; # *.sh
+          };
+        };
+      };
+    };
 }
