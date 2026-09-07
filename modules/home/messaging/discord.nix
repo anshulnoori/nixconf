0620469339a4c371c388497492b0a5982dc52f4f
Anshul Noori <anshulnoori@gmail.com>
2026-09-07 03:28:15 +0000

    fix(discord): disable Stylix theme

--- modules/home/messaging/discord.nix
+++ modules/home/messaging/discord.nix
@@ -6,21 +6,20 @@ _: {
     ...
   }: {
     home.packages = [(pkgs.discord-canary.override {withVencord = true;})];
-    stylix.targets.vencord.enable = true;
+    stylix.targets.vencord.enable = false;
     xdg.mimeApps = {
       enable = true;
       defaultApplications."x-scheme-handler/discord" = ["discord-canary.desktop"];
     };
 
-    home.activation.enableVencordStylix = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    home.activation.disableVencordStylix = lib.hm.dag.entryAfter ["writeBoundary"] ''
       settings_dir=${lib.escapeShellArg "${config.xdg.configHome}/Vencord/settings"}
       settings_file="$settings_dir/settings.json"
 
-      run mkdir -p "$settings_dir"
       if [[ -f "$settings_file" ]]; then
         temporary="$(${pkgs.coreutils}/bin/mktemp "$settings_dir/settings.json.XXXXXX")"
         if ${pkgs.jq}/bin/jq \
-          '.enabledThemes = ((.enabledThemes // []) + ["stylix.theme.css"] | unique)' \
+          'if .enabledThemes then .enabledThemes -= ["stylix.theme.css"] else . end' \
           "$settings_file" > "$temporary"; then
           if ! ${pkgs.diffutils}/bin/cmp -s "$settings_file" "$temporary"; then
             run mv "$temporary" "$settings_file"
@@ -31,8 +30,6 @@ _: {
           run rm "$temporary"
           echo "warning: Vencord settings are not valid JSON; leaving them unchanged" >&2
         fi
-      else
-        printf '%s\n' '{"enabledThemes":["stylix.theme.css"]}' > "$settings_file"
       fi
     '';
   };
