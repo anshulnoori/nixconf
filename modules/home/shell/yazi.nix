586b52bf9a09d716a6276db9e0cb86c1289f8cf3
Anshul Noori <anshulnoori@gmail.com>
2026-09-07 02:06:40 +0000

    fix(desktop): float Yazi chooser and use q to cancel

--- modules/home/shell/yazi.nix
+++ modules/home/shell/yazi.nix
@@ -24,7 +24,8 @@ _: {
       name = "yazi-file-chooser";
       runtimeInputs = [config.programs.yazi.finalPackage pkgs.kitty pkgs.gnused pkgs.coreutils pkgs.bash];
       text = ''
-        export TERMCMD='${pkgs.kitty}/bin/kitty --title termfilechooser'
+        # Reuse present-terminal's floating rule, without its pause or detached launch.
+        export TERMCMD='${pkgs.kitty}/bin/kitty --class TUI.float --title termfilechooser'
         exec ${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh "$@"
       '';
     };
@@ -32,23 +33,23 @@ _: {
     stylix.targets.yazi.enable = true;
 
     programs.yazi = {
-      plugins.chooser-escape = pkgs.writeTextDir "main.lua" ''
+      plugins.chooser-quit = pkgs.writeTextDir "main.lua" ''
         ---@sync entry
         return {
           entry = function()
             if rt.args.chooser_file then
               ya.emit("quit", { no_cwd_file = true })
             else
-              ya.emit("escape", {})
+              ya.emit("quit", {})
             end
           end,
         }
       '';
       keymap.mgr.prepend_keymap = [
         {
-          on = "<Esc>";
-          run = "plugin chooser-escape";
-          desc = "Cancel chooser, otherwise escape";
+          on = "q";
+          run = "plugin chooser-quit";
+          desc = "Cancel chooser, otherwise quit";
         }
       ];
     };
